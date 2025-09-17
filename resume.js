82ff5ef96aeef7d8bc191c4c83594b367a4f225d
lukersere <lukersere@gmail.com>
2025-09-17 09:07:28 -0500

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,17 @@
+// Define Variables
+let myName = "Luke Jones";
+let myEmail = "Lukersere@gmail.com";
+let title = "Student";
+let experience = "Food Service, Retail, Graphic Design";
+let education = "Oral Roberts University";
+let skills = ["Teamleading", "Extroversion", "Communication"];
+// let skills = "something"; //TODO
+
+//connect variables to HTML (ligaments)
+
+
+document.getElementById("my-name").textContent = myName;
+document.getElementById("my-email").textContent = myEmail;
+document.getElementById("title").textContent = title;
+document.getElementById("experience").textContent = experience;
+document.getElementById("education").textContent = education;
